fc566ec07f0caa1608458ea8703d758a213cdfcb
AntoineB33 <antoine.barbault@gmail.com>
2025-10-07 02:31:43 +0200

    Fix rclone command in save-to-cloud.bat to use sync instead of copy

--- save-to-cloud.bat
+++ save-to-cloud.bat
@@ -5,6 +5,6 @@ set "REMOTE=gdrive:/backup-folder"
 set "FILTERFILE=C:\Users\N6506\Home\computer\rcloning\data\sync-filter.txt"
 
 :: Run rclone with proper quoting
-rclone copy "%LOCALFOLDER%" "%REMOTE%" --filter-from "%FILTERFILE%" --create-empty-src-dirs --progress
+rclone sync "%LOCALFOLDER%" "%REMOTE%" --filter-from "%FILTERFILE%" --create-empty-src-dirs --progress
 
 pause
